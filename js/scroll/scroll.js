9e3a9ec78636d4ffd99eac2d18a9c48f9b0a3693
ilja <ishelganov@mail.ru>
2016-09-22 01:00:15 +0300

    add animated header

--- js/scroll/scroll.js
+++ js/scroll/scroll.js
@@ -1,13 +1,40 @@
 $(function () {
+
+    // Верхний паддинг у блока promo
+    var promo_paddingTop = parseInt($(".promo").css("padding-top"), 10);
+
     function Anchor() {
         var obj = $(this);
         $("html, body").animate(
-            {scrollTop: $(obj.attr("href")).offset().top}, 500)
-    };
+            {scrollTop: $(obj.attr("href")).offset().top - promo_paddingTop}, 500)
+    }
 
-    // function scrollHeader() {
-    //     var scroll = scrollTop().offset().top;
-    // }
+    $(window).scroll(function () {
+        var position = $(document).scrollTop();
+        var headerHeight = $("header").innerHeight();
+
+        // Обнуляем все если шапка на своем месте
+        if (position == 0) {
+            $("header").removeClass("header--fixed hidden visible");
+            $(".promo").css({'padding-top': promo_paddingTop});
+        }
+
+        if (position > headerHeight) {
+            $('header').addClass("hidden");
+        }
+
+        if (position > 200) {
+            $("header").removeClass("hidden");
+            $("header").addClass("header--fixed visible");
+            $(".promo").css({'padding-top': promo_paddingTop + headerHeight});
+        }
+
+
+        var featuresHeight = $(".features").height();
+        // console.log("position: " + position);
+        // console.log(headerHeight);
+
+    })
 
     $("a").click(Anchor);
 
